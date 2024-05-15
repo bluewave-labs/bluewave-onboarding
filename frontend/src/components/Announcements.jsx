dbf2ff9a253b7aef72ac2df46072d625e7c240e9
erenfn <erenfindik7@gmail.com>
2024-05-15 22:27:08 +0300

    finished announcememts, progressSteps, settings, fileUpload-except progress bar

---
+++ frontend/src/components/Announcements.jsx
@@ -0,0 +1,36 @@
+import {useState, React} from 'react';
+import styles from '../styles/Announcements.module.scss';
+
+const Announcements = () => {
+    const [showFirstSection, setShowFirstSection] = useState(false);
+
+    return (
+        <div className={styles.container}>
+            <h2>We've just released a new update</h2>
+            <h3>Check out all new dashboard view. Pages and now load faster </h3>
+
+            {showFirstSection && (
+                <div className={styles.buttons}>
+                    <button className={styles.dismiss}>Dismiss</button>
+                    <button className={styles.changelog}>Changelog</button>
+                </div>
+            )}
+
+            {!showFirstSection && (
+                <div> 
+                    <h3 style={{fontWeight: 600}}>Subscribe to updates</h3>
+                    <div className={styles.buttons}>
+                        <input
+                            type="text"
+                            placeholder="Enter your email"
+                            className={styles.emailInput}
+                        />
+                        <button className={styles.changelog}>Subscribe</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Announcements;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import ProgressSteps from './components/ProgressSteps';
+import Announcements from './components/Announcements';
+import Settings from './components/Settings';
+import FileUpload from './components/FileUpload';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FileUpload />
     </BrowserRouter>
   </React.StrictMode>
 );
