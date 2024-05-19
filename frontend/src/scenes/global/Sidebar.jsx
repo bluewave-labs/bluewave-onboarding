b375822abbab99adb38d522ddb389ada9d1a44ec
SimerdeepSinghGrewal <grewal_smart@yahoo.com>
2024-05-18 20:16:45 -0600

    added dropdown search-box with multi-select option

--- frontend/src/scenes/global/Sidebar.jsx
+++ frontend/src/scenes/global/Sidebar.jsx
@@ -1,30 +1,66 @@
-import React from 'react';
-import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import React, { useState } from 'react';
+import { List, ListItemButton, ListItemIcon, ListItemText, TextField, Autocomplete, Box, Chip } from '@mui/material';
 import { Home, BarChart, Settings } from '@mui/icons-material';
+import { demoData } from '../../data/demoData';
+import './SidebarStyles.css';
+
 
 const Sidebar = () => {
+    const [selectedItems, setSelectedItems] = useState([]);
+  
     return (
-        <List >
-            <ListItemButton>
-                <ListItemIcon>
-                    <Home />
-                </ListItemIcon>
-                <ListItemText primary="Dashboard" />
-            </ListItemButton>
-            <ListItemButton>
-                <ListItemIcon>
-                    <BarChart />
-                </ListItemIcon>
-                <ListItemText primary="Popups" />
-            </ListItemButton>
-            <ListItemButton>
-                <ListItemIcon>
-                    <Settings />
-                </ListItemIcon>
-                <ListItemText primary="Settings" />
-            </ListItemButton>
+      <Box>
+        <Autocomplete
+          multiple
+          id="search-box"
+          options={demoData}
+          getOptionLabel={(option) => option.name}
+          value={selectedItems}
+          onChange={(event, newValue) => {
+            setSelectedItems(newValue);
+          }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="outlined"
+              placeholder="Search"
+              className='dropDown_search'
+            />
+          )}
+          renderTags={(tagValue, getTagProps) =>
+            tagValue.map((option, index) => (
+              <Chip
+                label={option.name}
+                {...getTagProps({ index })}
+                key={option.id}
+              />
+            ))
+          }
+          disableCloseOnSelect
+          popupIcon={null}
+        />
+        <List>
+          <ListItemButton>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <BarChart />
+            </ListItemIcon>
+            <ListItemText primary="Popups" />
+          </ListItemButton>
+          <ListItemButton>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
         </List>
+      </Box>
     );
-};
-
-export default Sidebar;
+  };
+  
+  export default Sidebar;
