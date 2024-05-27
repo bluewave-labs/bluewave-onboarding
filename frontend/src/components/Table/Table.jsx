b17a6d8303de29e78c7e5160196eb9159cf8f6f2
SimerdeepSinghGrewal <grewal_smart@yahoo.com>
2024-05-26 19:08:17 -0600

    reduced file size of table.jsx

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import {
-  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, Popover, Checkbox, FormControlLabel
+  TableContainer, Table, TableHead, TableBody, TableCell, TableRow, Paper, Button, Popover, Checkbox, FormControlLabel
 } from '@mui/material';
 import { Tune, ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material';
 import './TableStyles.css';
 
 const DataTable = ({ data }) => {
   const [page, setPage] = useState(0);
-  const [rowsPerPage] = useState(10);
+  const rowsPerPage = 10;
   const [filterOptions, setFilterOptions] = useState({
     name: true,
     status: true,
@@ -39,14 +39,8 @@ const DataTable = ({ data }) => {
   };
 
   const filteredData = data;
-
   const pageCount = Math.ceil(filteredData.length / rowsPerPage);
   const displayData = filteredData.slice(page * rowsPerPage, (page + 1) * rowsPerPage);
-
-  const pageNumbers = [];
-  for (let i = 0; i < pageCount; i++) {
-    pageNumbers.push(i);
-  }
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
@@ -65,142 +59,54 @@ const DataTable = ({ data }) => {
         open={open}
         anchorEl={anchorEl}
         onClose={handleClosePopover}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
       >
         <div className="popoverContent">
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.name}
-                onChange={handleFilterChange}
-                name="name"
-              />
-            }
-            label="Name"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.status}
-                onChange={handleFilterChange}
-                name="status"
-              />
-            }
-            label="Status"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.role}
-                onChange={handleFilterChange}
-                name="role"
-              />
-            }
-            label="Role"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.team}
-                onChange={handleFilterChange}
-                name="team"
-              />
-            }
-            label="Team"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.hireDate}
-                onChange={handleFilterChange}
-                name="hireDate"
-              />
-            }
-            label="Hire Date"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.employeeNo}
-                onChange={handleFilterChange}
-                name="employeeNo"
-              />
-            }
-            label="Employee No"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={filterOptions.employmentStatus}
-                onChange={handleFilterChange}
-                name="employmentStatus"
-              />
-            }
-            label="Employment Status"
-          />
+          {Object.entries(filterOptions).map(([key, value]) => (
+            <FormControlLabel
+              key={key}
+              control={<Checkbox checked={value} onChange={handleFilterChange} name={key} />}
+              label={key.charAt(0).toUpperCase() + key.slice(1)}
+            />
+          ))}
         </div>
       </Popover>
-      
+
       <TableContainer component={Paper}>
         <Table>
           <TableHead className='tableHeadRow'>
             <TableRow>
-              {filterOptions.name && <TableCell className='tableCell'>Name</TableCell>}
-              {filterOptions.status && <TableCell className='tableCell'>Status</TableCell>}
-              {filterOptions.role && <TableCell className='tableCell'>Role</TableCell>}
-              {filterOptions.team && <TableCell className='tableCell'>Team</TableCell>}
-              {filterOptions.hireDate && <TableCell className='tableCell'>Hire Date</TableCell>}
-              {filterOptions.employeeNo && <TableCell className='tableCell'>Employee No</TableCell>}
-              {filterOptions.employmentStatus && <TableCell className='tableCell'>Employment Status</TableCell>}
+              {Object.entries(filterOptions).map(([key, value]) => (
+                value && <TableCell key={key} className='tableCell'>{key.charAt(0).toUpperCase() + key.slice(1)}</TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
             {displayData.map(row => (
               <TableRow key={row.id}>
-                {filterOptions.name && <TableCell className='tableCell'>{row.name}</TableCell>}
-                {filterOptions.status && <TableCell className='tableCell'>{row.status}</TableCell>}
-                {filterOptions.role && <TableCell className='tableCell'>{row.role}</TableCell>}
-                {filterOptions.team && <TableCell className='tableCell'>{row.team}</TableCell>}
-                {filterOptions.hireDate && <TableCell className='tableCell'>{row.hireDate}</TableCell>}
-                {filterOptions.employeeNo && <TableCell className='tableCell'>{row.employeeNo}</TableCell>}
-                {filterOptions.employmentStatus && <TableCell className='tableCell'>{row.employmentStatus}</TableCell>}
+                {Object.entries(filterOptions).map(([key, value]) => (
+                  value && <TableCell key={key} className='tableCell'>{row[key]}</TableCell>
+                ))}
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+
       <div className="paginationContainer">
-        <button className="direct_buttons"
-          onClick={() => handleChangePage(page - 1)}
-          disabled={page === 0}
-        >
-          <ArrowBackIosNew />
-          Previous
+        <button className="direct_buttons" onClick={() => handleChangePage(page - 1)} disabled={page === 0}>
+          <ArrowBackIosNew />Previous
         </button>
         <div className="paginationNumbers">
-          {pageNumbers.map((pageNumber) => (
-            <button
-              key={pageNumber}
-              onClick={() => handleChangePage(pageNumber)}
-              disabled={pageNumber === page}
-            >
+          {Array.from(Array(pageCount).keys()).map(pageNumber => (
+            <button key={pageNumber} onClick={() => handleChangePage(pageNumber)} disabled={pageNumber === page}>
               {pageNumber + 1}
             </button>
           ))}
         </div>
-        <button className="direct_buttons"
-          onClick={() => handleChangePage(page + 1)}
-          disabled={page >= pageCount - 1}
-        >
-          Next
-          <ArrowForwardIos />
+        <button className="direct_buttons" onClick={() => handleChangePage(page + 1)} disabled={page >= pageCount - 1}>
+          Next<ArrowForwardIos />
         </button>
       </div>
     </div>
