401d4614ec0c7a17bdc5f4982af72f45ac3035aa
SimerdeepSinghGrewal <grewal_smart@yahoo.com>
2024-06-01 17:45:26 -0600

    Issue 49:Refactor Title Component to accept props

--- frontend/src/components/Title/Title.jsx
+++ frontend/src/components/Title/Title.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Box, Button, Typography } from '@mui/material';
-import HeadingTabs from "../../components/Tabs/Tabs";
 import './TitleStyles.css';
 
-const Title = () => {
+const Title = ({ title, buttonText, onButtonClick, children }) => {
   return (
     <div className='titleContainer'>
-    <Box display="flex" justifyContent="space-between" alignItems="center" mb={3}>
-      <Typography variant="h5" component="div">
-        People
-      </Typography>
-      <Button className='add-new' variant="contained">
-        Add New Employees
-      </Button>
-    </Box>
-    <HeadingTabs/>
+      <Box display="flex" justifyContent="space-between" alignItems="center" mb={3}>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Button className='add-new' variant="contained" onClick={onButtonClick}>
+          {buttonText}
+        </Button>
+      </Box>
+      {children}
     </div>
   );
 };
