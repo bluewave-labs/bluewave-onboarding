17e7f63049b916e6506c5d43b9bc418c89fe44ed
Sevinj Feyziyeva <sevinjfeyziyeva@Sevinjs-MacBook-Air.local>
2024-06-11 08:46:50 -0400

    Removing LogoBW from Home

--- frontend/src/scenes/home/Home.jsx
+++ frontend/src/scenes/home/Home.jsx
@@ -1,16 +1,10 @@
 import HomePageTemplate from '../../components/templates/HomePageTemplate';
 import Dashboard from '../dashboard';
 import Logo from '../../components/Logo/Logo';
-import LogoBW from '../../assets/logo/introflow_logo_bw.svg'; 
+
 import "./Home.css"
 
 const Home = () => {
-    const logo = {
-        src: LogoBW,
-        alt: 'Introflow logo',
-        className: 'logo-item',
-      };
-      
     return (
         <div className="app">
             <div className="content">
