6df897e8d3b0d8078536c359d1eb82d2be11963e
erenfn <erenfindik7@gmail.com>
2024-06-21 09:38:09 -0400

    change sequelize synch

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ sequelize
   .catch((err) => console.log("Error: " + err));
 
 sequelize
-  .sync()
+  .sync({force:true})
   .then(() => console.log("Models synced with the database..."))
   .catch((err) => console.log("Error syncing models: " + err));
 
