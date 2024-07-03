d7103ca6226194617a26f1ec1316dd08f03a7a57
uparkalau <132397574+uparkalau@users.noreply.github.com>
2024-07-02 22:01:53 -0700

    fix for the createActivityButton

--- frontend/src/scenes/dashboard/Dashboard.jsx
+++ frontend/src/scenes/dashboard/Dashboard.jsx
@@ -4,19 +4,21 @@ import UserTitle from '../../components/HomePageComponents/UserTitle/UserTitle';
 import styles from "./Dashboard.module.scss";
 import StatisticCardList from '../../components/HomePageComponents/StatisticCardsList/StatisticCardsList';
 import CreateActivityButtonList from '../../components/HomePageComponents/CreateActivityButtonList/CreateActivityButtonList';
+import { useNavigate  } from 'react-router-dom';
 
 const Dashboard = ({ username }) => {
-  const metrics = [
-    { metricName: "Monthly active users", metricValue: 1000, changeRate: 10 },
-    { metricName: "Tour views", metricValue: 5000, changeRate: 5 },
-    { metricName: "Hint views", metricValue: 2000, changeRate: -20 },
-    { metricName: "Popup Views", metricValue: 3000, changeRate: 15 }
-  ];
+    const navigate = useNavigate();
+    const metrics = [
+        { metricName: "Monthly active users", metricValue: 1000, changeRate: 10 },
+        { metricName: "Tour views", metricValue: 5000, changeRate: 5 },
+        { metricName: "Hint views", metricValue: 2000, changeRate: -20 },
+        { metricName: "Popup Views", metricValue: 3000, changeRate: 15 }
+    ];
 
     const buttons = [
-        { placeholder: "Create a welcome tour"},
-        { placeholder: "Add a hint to your app" },
-        { placeholder: "Create a new banner"}
+        { placeholder: "Create a welcome tour", onClick: () => navigate('/welcome-tour-route') },
+        { placeholder: "Add a hint to your app", onClick:() => navigate('/hint-route') },
+        { placeholder: "Create a new banner", onClick:() => navigate('/new-banner-route') }
     ];
     return (
         <>
