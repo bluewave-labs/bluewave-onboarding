cd217bf5b7899529744a40d55b7e291e47a3e388
SimerdeepSinghGrewal <grewal_smart@yahoo.com>
2024-07-27 12:16:35 -0600

    Issue 75: change svg icon and use var colors

--- frontend/src/components/List/ListItem/ListItem.jsx
+++ frontend/src/components/List/ListItem/ListItem.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import SettingsIcon from '@mui/icons-material/Settings';
+import CircleIcon from '@mui/icons-material/Circle';
 import './ListItem.css';
 
 const ListItem = ({ title, text, id, onClick, onDelete, onEdit }) => {
@@ -10,9 +11,10 @@ const ListItem = ({ title, text, id, onClick, onDelete, onEdit }) => {
     <div className="list-item" onClick={onClick}>
       <div className="list-item-info">
         <div className="list-item-header">
-          <svg width="16" height="16" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg" className="list-item-icon">
-            <path d="M0 3C0 1.34315 1.34315 0 3 0V0C4.65685 0 6 1.34315 6 3V3C6 4.65685 4.65685 6 3 6V6C1.34315 6 0 4.65685 0 3V3Z" fill="#7F56D9"/>
-          </svg>
+          <div className="list-item-icon-container">
+            <CircleIcon className="list-item-icon" />
+            <div className="list-item-dot"></div>
+          </div>
           <h4>{title}</h4>
         </div>
         {text && <p>{text}</p>}
