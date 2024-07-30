d1467715d089d525d1d53a0bf521f441047c06cd
Rusi Solanki <rusi.solanki04071998@gmail.com>
2024-07-30 16:51:27 -0400

    Change Format

--- frontend/src/components/DropdownList/DropdownList.jsx
+++ frontend/src/components/DropdownList/DropdownList.jsx
@@ -1,27 +1,25 @@
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import React from 'react'
-import PropTypes from 'prop-types'
-import './DropdownList.css'
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import React from "react";
+import PropTypes from "prop-types";
+import "./DropdownList.css";
 
-const DropdownList = ({actions = ['Example', 'Second Example']}) => {
-
+const DropdownList = ({ actions = [] }) => {
   return (
-    <div className='dropdown'>
-    <Select
-          defaultValue={actions[0]}
-          className='select'
-        >
-          {actions.map((action, index) => (
-            <MenuItem key={index} className='menuItem' value={action}>{action}</MenuItem>
-          ))}
-        </Select>
-        </div>
-  )
-}
+    <div className="dropdown">
+      <Select defaultValue={actions[0]} className="select">
+        {actions.map((action, index) => (
+          <MenuItem key={index} className="menuItem" value={action}>
+            {action}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  );
+};
 
 DropdownList.propTypes = {
-    actions: PropTypes.array
-}
+  actions: PropTypes.array,
+};
 
-export default DropdownList
+export default DropdownList;
