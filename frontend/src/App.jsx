6843f227dac556d3bbd3605c4747c1ba53a3e01a
thomastepi <tepexminem2013@gmail.com>
2024-08-18 02:50:38 -0400

    implement-create-new-hint-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import LinksDefaultPage from "./scenes/links/LinksDefaultPage";
 import ToursDefaultPage from "./scenes/tours/ToursDefaultPage";
 import PopupDefaultPage from "./scenes/popup/PopupDefaultPage";
 import HintDefaultPage from "./scenes/hints/HintDefaultPage";
+import HintPage from "./scenes/hints/HintPage";
 import CreatePopupPage from "./scenes/popup/CreatePopupPage";
 
 function App() {
@@ -23,10 +24,13 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={isLoggedIn ? <Private Component={Home} /> : <LoginPage />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Private Component={Home} /> : <LoginPage />}
+        />
         <Route path="/home" element={<Private Component={Home} />} />
-        <Route path="/" element={isLoggedIn ? <Home/> : <LoginPage />} />        
-        <Route path="/login" element={<LoginPage />} />        
+        <Route path="/" element={isLoggedIn ? <Home /> : <LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<CreateAccountPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/reset-password" element={<PasswordResetPage />} />
@@ -40,7 +44,8 @@ function App() {
         <Route path="/link" element={<LinksDefaultPage />} />
         <Route path="/tour" element={<ToursDefaultPage />} />
         <Route path="/hint" element={<HintDefaultPage />} />
-      </Routes>    
+        <Route path="/hint/create" element={<HintPage />} />
+      </Routes>
     </>
   );
 }
---
+++ frontend/src/components/HintPageComponents/HintLeftContent/HintLeftContent.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import DropdownList from "../../DropdownList/DropdownList";
+import CustomTextField from "../../TextFieldComponents/CustomTextField/CustomTextField";
+import "./HintLeftContent.css";
+
+const HintLeftContent = ({ setLeftContentFormData, formData }) => {
+  const handleChange = (name) => (e) => {
+    setLeftContentFormData({ ...formData, [name]: e.target.value });
+  };
+
+  return (
+    <div className="left-content-container">
+      <h2 className="hint-label">Action</h2>
+      <DropdownList
+        actions={["No action", "Open a URL", "Open a URL in a new page"]}
+      />
+      <h2 className="hint-label" style={{ marginBottom: 0 }}>
+        Action button url (can be relative)
+      </h2>
+      <CustomTextField
+        TextFieldWidth="241px"
+        value={formData.actionButtonUrl}
+        onChange={handleChange("actionButtonUrl")}
+      />
+      <h2 className="hint-label" style={{ marginBottom: 0 }}>
+        Action button text
+      </h2>
+      <CustomTextField
+        TextFieldWidth="241px"
+        value={formData.actionButtonText}
+        onChange={handleChange("actionButtonText")}
+      />
+      <h2 className="hint-label" style={{ marginBottom: 0 }}>
+        Target Element
+      </h2>
+      <CustomTextField
+        TextFieldWidth="241px"
+        value={formData.targetElement}
+        onChange={handleChange("targetElement")}
+        helperText="Page element to attach tooltip to"
+      />
+      <h2 className="hint-label">Tooltip Placement</h2>
+      <DropdownList actions={["Top", "Right", "Bottom", "Left"]} />
+    </div>
+  );
+};
+
+export default HintLeftContent;
