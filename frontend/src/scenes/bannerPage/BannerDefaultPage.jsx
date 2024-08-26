aa396847da5228c94ad77b3ff5de162892dbfd7c
erenfn <erenfindik7@gmail.com>
2024-08-26 15:20:35 +0300

    added readme for defaultpageTemplate

--- frontend/src/scenes/bannerPage/BannerDefaultPage.jsx
+++ frontend/src/scenes/bannerPage/BannerDefaultPage.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import DefaultPageTemplate from '../../templates/DefaultPageTemplate/DefaultPageTemplate';
 import { getBanners, deleteBanner } from '../../services/bannerServices';
 import { useNavigate } from 'react-router-dom';
-import { ACTIVITY_TYPES } from "../../data/CreateActivityButtonData";
 import { ACTIVITY_TYPES_INFO } from '../../data/GuideMainPageData';
 
 const BannerDefaultPage = () => {
@@ -22,7 +21,7 @@ const BannerDefaultPage = () => {
             fetchItems={getBanners}
             deleteItem={deleteBanner}
             navigateToCreate={navigateToCreate}
-            itemType={ACTIVITY_TYPES.BANNERS}
+            itemType={ACTIVITY_TYPES_INFO.BANNERS}
             itemTypeInfo={ACTIVITY_TYPES_INFO.BANNERS}
             getItemDetails={getBannerDetails}
         />
