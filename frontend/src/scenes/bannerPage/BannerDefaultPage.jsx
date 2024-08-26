d34b267a4c2556f8b496123f7e6f05f4bcffe80b
erenfn <erenfindik7@gmail.com>
2024-08-26 15:08:04 +0300

    create default page template

--- frontend/src/scenes/bannerPage/BannerDefaultPage.jsx
+++ frontend/src/scenes/bannerPage/BannerDefaultPage.jsx
@@ -1,84 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import CreateActivityButton from "../../components/Button/CreateActivityButton/CreateActivityButton";
-import HomePageTemplate from "../../templates/HomePageTemplate/HomePageTemplate";
-import { ACTIVITY_TYPES } from "../../data/CreateActivityButtonData";
-import ParagraphCSS from "../../components/ParagraphCSS/ParagraphCSS";
+import React from 'react';
+import DefaultPageTemplate from '../../templates/DefaultPageTemplate/DefaultPageTemplate';
+import { getBanners, deleteBanner } from '../../services/bannerServices';
 import { useNavigate } from 'react-router-dom';
-import GuideMainPageTemplate from "../../templates/GuideMainPageTemplate/GuideMainPageTemplate";
-import { getBanners } from "../../services/bannerServices";
+import { ACTIVITY_TYPES } from "../../data/CreateActivityButtonData";
 import { ACTIVITY_TYPES_INFO } from '../../data/GuideMainPageData';
-import { deleteBanner } from '../../services/bannerServices';
 
 const BannerDefaultPage = () => {
     const navigate = useNavigate();
-    const [banners, setBanners] = useState([]);
-    const [isPopupOpen, setPopupOpen] = useState(false);
-    const [bannerToDelete, setBannerToDelete] = useState();
-    const [bannerDeleted, setBannerDeleted] = useState(false);
 
-    const handleDelete = async () => {
-        await deleteBanner(bannerToDelete)
-        setPopupOpen(false);
-        setBannerDeleted(prevState => !prevState);
+    const getBannerDetails = (banner) => ({
+        title: `Banner ${banner.id}`,
+        text: banner.bannerText,
+    });
+
+    const navigateToCreate = (state) => {
+        navigate('/banner/create', state);
     };
-
-    const handleOpenPopup = (id) => {
-        setBannerToDelete(id)
-        setPopupOpen(true);
-    };
-
-    const handleClosePopup = () => {
-        setPopupOpen(false);
-    };
-
-    const navigateToCreate = () => {navigate('/banner/create')}
-
-    useEffect(() => {
-        const fetchBanners = async () => {
-            try {
-                const bannerData = await getBanners();
-                setBanners(bannerData);
-            } catch (error) {
-                console.error("Failed to fetch banners:", error);
-            }
-        };
-
-        fetchBanners();
-    }, [bannerDeleted]);
-
-    const style = {
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-    };
-
-    const items = banners.map(banners => ({
-        idItem: banners.id,
-        title: `Banner ${banners.id}`,
-        text: banners.bannerText,
-        onDelete: () => handleOpenPopup(banners.id), 
-        onEdit: () => navigate('/banner/create', {state:{isEdit:true, id: banners.id}}),
-      }));
 
     return (
-        <HomePageTemplate>
-            {banners.length === 0 ? (
-                <div style={style}>
-                    <ParagraphCSS />
-                    <CreateActivityButton type={ACTIVITY_TYPES.BANNERS} onClick={navigateToCreate} />
-                </div>
-            ) : (
-                <GuideMainPageTemplate items={items}
-                    handleDelete={handleDelete}
-                    isPopupOpen={isPopupOpen}
-                    handleClosePopup={handleClosePopup} 
-                    type={ACTIVITY_TYPES_INFO.BANNERS}
-                    onClick={navigateToCreate}/>
-            )}
-        </HomePageTemplate>
+        <DefaultPageTemplate
+            fetchItems={getBanners}
+            deleteItem={deleteBanner}
+            navigateToCreate={navigateToCreate}
+            itemType={ACTIVITY_TYPES.BANNERS}
+            itemTypeInfo={ACTIVITY_TYPES_INFO.BANNERS}
+            getItemDetails={getBannerDetails}
+        />
     );
-}
+};
 
 export default BannerDefaultPage;
