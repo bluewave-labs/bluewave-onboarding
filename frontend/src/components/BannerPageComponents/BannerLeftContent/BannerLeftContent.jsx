1fc0504fe0dd09b34a1fdcb435f45ab0c506c039
SimerdeepSinghGrewal <grewal_smart@yahoo.com>
2024-08-27 13:43:04 -0600

    RadioButton: update bannerLeftContent for functionality and update styles according to figma

--- frontend/src/components/BannerPageComponents/BannerLeftContent/BannerLeftContent.jsx
+++ frontend/src/components/BannerPageComponents/BannerLeftContent/BannerLeftContent.jsx
@@ -1,17 +1,22 @@
-import { React } from 'react';
+import React, { useState } from 'react';
 import styles from './BannerLeftContent.module.scss';
 import DropdownList from '../../DropdownList/DropdownList';
-import { Radio, FormControlLabel } from "@mui/material";
-import {RadioGroup} from '@mui/material';
 import CustomTextField from '../../TextFieldComponents/CustomTextField/CustomTextField';
 import RadioButton from '../../RadioButton/RadioButton';
 
 const BannerLeftContent = ({ setIsTopPosition, url, setUrl, setButtonAction }) => {
+    const [position, setPosition] = useState('top');
     const handleSetUrl = (event) => {
         setUrl(event.target.value);
     };
+
     const handleActionChange = (newAction) => {
         setButtonAction(newAction);
+    };
+
+    const handlePositionChange = (newPosition) => {
+        setPosition(newPosition);
+        setIsTopPosition(newPosition === 'top');
     };
 
     return (
@@ -22,33 +27,33 @@ const BannerLeftContent = ({ setIsTopPosition, url, setUrl, setButtonAction }) =
                 onActionChange={handleActionChange}
             />
             <h2>Position</h2>
-
-            {/* How the functionality should be */}
-            <RadioGroup
-                defaultValue="top"
-                name="radio-buttons-group"
-            >
-                <FormControlLabel value="top" control={<Radio onClick={() => setIsTopPosition(true)}/>} label="Top" />
-                <FormControlLabel value="bottom" control={<Radio onClick={() => setIsTopPosition(false)}/>} label="Bottom" />
-            </RadioGroup>
-            {/* How it should look */}
             <div className={styles.radioContent}>
-                <Radio onClick={() => setIsTopPosition(true)} />
-                <h3>Top (centered)</h3>
+                <RadioButton
+                    id="top-centered-position"
+                    name="position"
+                    value="top-centered"
+                    label="Top (centered)"
+                    checked={position === 'top'}
+                    onChange={() => handlePositionChange('top')}
+                />
             </div>
             <div className={styles.radioContent}>
-                <Radio onClick={() => setIsTopPosition(false)} />
-                <h3>Bottom (centered)</h3>
+                <RadioButton
+                    id="bottom-centered-position"
+                    name="position"
+                    value="bottom-centered"
+                    label="Bottom (centered)"
+                    checked={position === 'bottom'}
+                    onChange={() => handlePositionChange('bottom')}
+                />
             </div>
-            {/* How it is right now */}
-            <RadioButton name='top-bottom' label='top' onClick={() => setIsTopPosition(true)}/>
-            <RadioButton name='top-bottom' label='bottom' onClick={() => setIsTopPosition(false)}/>
 
             <h2 style={{ marginBottom: 0 }}>URL</h2>
             <CustomTextField
-                TextFieldWidth='241px'
+                TextFieldWidth="241px"
                 value={url}
-                onChange={handleSetUrl} />
+                onChange={handleSetUrl}
+            />
         </div>
     );
 };
--- frontend/src/components/RadioButton/RadioButton.jsx
+++ frontend/src/components/RadioButton/RadioButton.jsx
@@ -1,60 +1,77 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Radio } from "@mui/material";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Radio } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import './RadioButtonStyles.css';
+const CustomRadioIcon = styled('span')({
+    borderRadius: '50%',
+    width: 16,
+    height: 16,
+    boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+  });
 
-const RadioButton = ({
-    id,
-    name,
-    value,
-    label,
-    onChange,
-    onclick,
-    sx,
-    buttonSize = 'small',
-    color = 'var(--main-purple)',
-    checked = false,
-    enabled = true,
-    
-}) => {
-    const sizeClass = buttonSize === 'large' ? 'radio-large' : 'radio-small';
-    
-    const handleChange = (event) => {
-        if (checked) {
-            onChange({ target: { name, value: '' } });
-        } else {
-            onChange(event);
-        }
-    };
+const CustomRadioCheckedIcon = styled('span')({
+    borderRadius: '50%',
+    backgroundColor: 'var(--main-purple)',
+  '&::before': {
+    display: 'block',
+    width: 16,
+    height: 16,
+    backgroundImage: 'radial-gradient(#fff, #fff 28%, transparent 32%)',
+    content: '""',
+  },
+});
 
-    return (
-        <div className={`radio-button ${sizeClass}`}>
-            <Radio                
-                id={id}                
-                name={name}
-                value={value}            
-                disabled={!enabled}
-                onChange={handleChange}
-                size={buttonSize === 'large' ? 'large' : 'small'}
-                style={{ color: color }}
-                checked={checked}
-                onclick={onclick}
-                sx={sx}
-            />
-            {label && <label htmlFor={id}>{label}</label>}
-        </div>
-    );
-};
+function RadioButton({ 
+    id, 
+    name, 
+    value, 
+    label, 
+    onChange, 
+    onclick, 
+    buttonSize = 'small', 
+    color = 'var(--main-purple)', 
+    checked = false, 
+    enabled = true 
+}) {
+  const sizeClass = buttonSize === 'large' ? 'radio-large' : 'radio-small';
+
+  const handleChange = (event) => {
+    if (checked) {
+      onChange({ target: { name, value: '' } });
+    } else {
+      onChange(event);
+    }
+  };
+
+  return (
+    <div className={`radio-button ${sizeClass}`}>
+      <Radio
+        id={id}
+        name={name}
+        value={value}
+        disabled={!enabled}
+        onChange={handleChange}
+        size={buttonSize === 'large' ? 'large' : 'small'}
+        checkedIcon={<CustomRadioCheckedIcon />}
+        icon={<CustomRadioIcon />}
+        checked={checked}
+        onClick={onclick}
+      />
+      {label && <label htmlFor={id}>{label}</label>}
+    </div>
+  );
+}
 
 RadioButton.propTypes = {
-    id: PropTypes.string,
-    name: PropTypes.string,
-    value: PropTypes.string,
-    label: PropTypes.string,
-    enabled: PropTypes.bool,
-    onChange: PropTypes.func,
-    color: PropTypes.string,
-    checked: PropTypes.bool,
+  id: PropTypes.string,
+  name: PropTypes.string,
+  value: PropTypes.string,
+  label: PropTypes.string,
+  enabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  color: PropTypes.string,
+  checked: PropTypes.bool,
 };
 
 export default RadioButton;
