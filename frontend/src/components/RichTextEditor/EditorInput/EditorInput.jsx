8a4c512916823f25659290a20e61e34a95c34de3
thomastepi <tepexminem2013@gmail.com>
2024-08-29 08:28:23 -0400

    fix: update import path for Quill CSS in RichTextEditor

--- frontend/src/components/RichTextEditor/EditorInput/EditorInput.jsx
+++ frontend/src/components/RichTextEditor/EditorInput/EditorInput.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactQuill from "react-quill-new";
-import "react-quill/dist/quill.snow.css";
+import "react-quill-new/dist/quill.snow.css";
 import "./EditorInput.css";
 
 const EditorInput = ({ value, onChange, modules }) => {
