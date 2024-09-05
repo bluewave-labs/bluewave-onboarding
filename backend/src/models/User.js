b15b50631f7d15b59d2fa3a2b5f92b42aa6d8484
mr-loop-1 <loopolecreator1@gmail.com>
2024-09-05 10:17:30 +0530

    add surname field on signup page

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -10,7 +10,6 @@ module.exports = (sequelize, DataTypes) => {
       username: {
         type: DataTypes.STRING(50),
         allowNull: false,
-        unique: true,
       },
       email: {
         type: DataTypes.STRING(100),
