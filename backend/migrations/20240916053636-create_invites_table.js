963c8fea8b8c8c3b101eb13884de9d1100a3e819
mr-loop-1 <loopolecreator1@gmail.com>
2024-09-16 11:15:46 +0530

    add invites table

---
+++ backend/migrations/20240916053636-create_invites_table.js
@@ -0,0 +1,61 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("invites", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      invitedBy: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      invitee: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      role: {
+        type: Sequelize.INTEGER,       // to be made enum
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.INTEGER,       // to be made enum
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("invites");
+  },
+};
