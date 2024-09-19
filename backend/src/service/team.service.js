0356bf3ee10f489a7ef00ebb59acca1c83e99fd4
mr-loop-1 <loopolecreator1@gmail.com>
2024-09-20 00:45:13 +0530

    added team service fn experimental

--- backend/src/service/team.service.js
+++ backend/src/service/team.service.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const { TEAM } = require("../utils/constants");
 const Team = db.Team
 
 class UserService {
@@ -10,8 +11,7 @@ class UserService {
                     model: User,
                     where: { id: userId }
                 }],
-            })
-
+            });
             return team;
         }
         catch(err) {
@@ -19,13 +19,58 @@ class UserService {
         }
     }
     async getTeams(userId) {
-
+        try {
+            const teams = await User.findAll({
+                where: { id: userId },
+                include: [{
+                    model: Team
+                }]
+            });
+            return teams;
+        }
+        catch(err) {
+            throw new Error("Error retrieving Teams");
+        }
     }
     async createTeam({ userId, name }) {
-
+        try {
+            const team = await Team.create({
+                name: name,
+                createdBy: userId,
+                Users: [{
+                    userId: userId,
+                    role: TEAM.ROLE.ADMIN
+                }]
+            }, {
+                include: {
+                    model: User
+                }
+            })
+            return team;
+        }
+        catch(err) {
+            throw new Error("Error retrieving Teams");
+        }
     }
     async updateTeam({ userId, teamId, name }) {
-
+        try {
+            const [rows] = await Team.update(
+                {name: name},
+                {
+                    where: {
+                        id: teamId,
+                        '$Users.id': userId
+                    },
+                    include: [{
+                        model: User,
+                    }],
+                    returning: true
+                }
+            )
+        }
+        catch(err) {
+            throw new Error("Error updating Team");
+        }
     }
 }
 
