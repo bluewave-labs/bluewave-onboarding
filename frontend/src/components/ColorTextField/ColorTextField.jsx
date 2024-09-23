47c5ca24e2623cebdbd5ce37514bfa2df1e85919
unknown <parthmanhas@gmail.com>
2024-09-23 08:12:06 +0530

    fix color picker styles to match existing ones

--- frontend/src/components/ColorTextField/ColorTextField.jsx
+++ frontend/src/components/ColorTextField/ColorTextField.jsx
@@ -1,9 +1,10 @@
+import styles from './ColorTextField.module.scss'
 import { React } from 'react';
 import { MuiColorInput } from 'mui-color-input'
 
 const ColorTextField = ({ onChange = () => null, value = null }) => {
     return (
-        <MuiColorInput format="hex" isAlphaHidden="true" value={value} onChange={onChange} />
+        <MuiColorInput className={styles.colorTextField} format="hex" isAlphaHidden="true" value={value} onChange={onChange} />
     )
 }
 
