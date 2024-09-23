6bfe849414adc960e79211c0a42f85cf70835d44
SimerdeepSinghGrewal <grewal_smart@yahoo.com>
2024-09-23 15:12:11 -0600

    edit name of controller file and add postman collection to backend folder

--- backend/src/routes/tour.routes.js
+++ backend/src/routes/tour.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const tourController = require("../controllers/tourController");
+const tourController = require("../controllers/tour.Controller");
 const authenticateJWT = require("../middleware/auth.middleware");
 
-router.get("/tours", authenticateJWT, tourController.getAllTours);
-router.post("/tours", authenticateJWT, tourController.createTour);
-router.put("/tours/:id", authenticateJWT, tourController.updateTour);
-router.delete("/tours/:id", authenticateJWT, tourController.deleteTour);
+router.get("/", authenticateJWT, tourController.getAllTours);
+router.post("/", authenticateJWT, tourController.createTour);
+router.put("/:id", authenticateJWT, tourController.updateTour);
+router.delete("/:id", authenticateJWT, tourController.deleteTour);
 
 module.exports = router;
