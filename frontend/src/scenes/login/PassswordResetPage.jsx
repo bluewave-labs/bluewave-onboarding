f4f8b204113f3fcc0e0792eed4bb48448383cca3
Akash Das <76689571+akash70629@users.noreply.github.com>
2024-10-05 18:28:44 +0530

    Fixed typo

--- frontend/src/scenes/login/PassswordResetPage.jsx
+++ frontend/src/scenes/login/PassswordResetPage.jsx
@@ -9,7 +9,7 @@ function PasswordResetPage() {
     <div className="login-container">
       <h2 style={{marginBottom: "0px"}}>Password reset</h2>
       <h3>Your password has been successfully reset. Click below to log in manually.</h3>
-      <button className="sign-in-button" style={{marginRTop: "20px"}} onClick={() => navigate('/')}>
+      <button className="sign-in-button" style={{marginTop: "20px"}} onClick={() => navigate('/')}>
         Continue
       </button>
       <button className="back-to-login-button"> <ArrowBackIcon style={{fontSize: "18px", marginRight: "5px"}}/>Back to log in</button>
