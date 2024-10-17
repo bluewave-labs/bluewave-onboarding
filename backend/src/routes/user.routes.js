3d8540e3e033c4cd082d0677cc2c3a6328bb5697
parthmanhas <parthmanhas@gmail.com>
2024-10-18 02:19:04 +0530

    add back delete user route

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -5,7 +5,8 @@ const {
     updateUserDetails,
     checkAtLeastOneField,
     validateProfileUpdate,
-    handleValidationErrors
+    handleValidationErrors,
+    deleteUser
 } = require("../controllers/user.controller");
 const authenticateJWT = require("../middleware/auth.middleware");
 
@@ -14,5 +15,6 @@ const router = express.Router();
 router.get("/users-list", getUsersList);
 router.get("/current-user", authenticateJWT, getCurrentUser);
 router.put("/update-profile", authenticateJWT, checkAtLeastOneField, validateProfileUpdate, handleValidationErrors, updateUserDetails);
+router.delete("/delete", authenticateJWT, deleteUser);
 
 module.exports = router;
