be9516b2c14b387f8376a7530d98c90bae95e6f9
Debora Serra <debora.r.serra@gmail.com>
2024-10-25 09:41:03 -0700

    feat: add link service

---
+++ backend/src/service/link.service.js
@@ -0,0 +1,52 @@
+const db = require("../models");
+const Link = db.Link;
+
+class LinkService {
+  async getAllLinks() {
+    return await Link.findAll({
+      include: [{ model: db.User, as: "creator" }],
+    });
+  }
+
+  async getLinks(userId) {
+    return await Link.findAll({
+      where: {
+        createdBy: userId,
+      },
+      include: [{ model: db.User, as: "creator" }],
+    });
+  }
+
+  async createLink(data) {
+    return await Link.create(data);
+  }
+
+  async deleteLink(id) {
+    const rowsAffected = await Link.destroy({ where: { id } });
+    return rowsAffected !== 0;
+  }
+
+  async updateLink(id, data) {
+    const [affectedRows, updatedPopups] = await Link.update(data, {
+      where: { id },
+      returning: true,
+    });
+
+    if (affectedRows === 0) return null;
+
+    return updatedPopups[0];
+  }
+
+  async getLinkById(linkId) {
+    try {
+      return await Link.findOne({
+        where: { id: linkId },
+        include: [{ model: db.User, as: "creator" }],
+      });
+    } catch (error) {
+      throw new Error("Error retrieving link by ID");
+    }
+  }
+}
+
+module.exports = new LinkService();
