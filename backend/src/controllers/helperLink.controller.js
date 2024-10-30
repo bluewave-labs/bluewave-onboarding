c37804cf927bddc970ed1c5a81a8d8685a14bb78
Debora Serra <debora.r.serra@gmail.com>
2024-10-29 19:44:50 -0700

    feat: add helper link controller

---
+++ backend/src/controllers/helperLink.controller.js
@@ -0,0 +1,176 @@
+const helperService = require("../service/helperLink.service");
+const { internalServerError } = require("../utils/errors.helper");
+const { validateHexColor } = require("../utils/guide.helper");
+const { URL_REGEX } = require("../utils/link.helper");
+
+const validateUrl = (value) => {
+  return URL_REGEX.test(value);
+};
+
+class LinkController {
+  async addHelper(req, res) {
+    const userId = req.user.id;
+    const { title, headerBackgroundColor, linkFontColor, iconColor } = req.body;
+
+    if (!title) {
+      return res.status(400).json({
+        errors: [{ msg: "title is required" }],
+      });
+    }
+
+    if (
+      (headerBackgroundColor && !validateHexColor(headerBackgroundColor)) ||
+      (linkFontColor && !validateHexColor(linkFontColor)) ||
+      (iconColor && !validateHexColor(iconColor))
+    ) {
+      return res.status(400).json({
+        errors: [
+          {
+            msg: "Invalid value for headerBackgroundColor, linkFontColor or iconColor",
+          },
+        ],
+      });
+    }
+
+    try {
+      const newHelperData = {
+        ...req.body,
+        createdBy: userId,
+      };
+      const newHelper = await helperService.createHelper(newHelperData);
+      res.status(201).json(newHelper);
+    } catch (err) {
+      console.log(err);
+      const { statusCode, payload } = internalServerError(
+        "CREATE_HELPER_ERROR",
+        err.message
+      );
+      res.status(statusCode).json(payload);
+    }
+  }
+
+  async deleteHelper(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (isNaN(id) || id.trim() === "") {
+        return res.status(400).json({ errors: [{ msg: "Invalid id" }] });
+      }
+
+      const deletionResult = await helperService.deleteHelper(id);
+
+      if (!deletionResult) {
+        return res.status(404).json({
+          errors: [{ msg: "Helper with the specified id does not exist" }],
+        });
+      }
+
+      res
+        .status(200)
+        .json({ message: `Helper with ID ${id} deleted successfully` });
+    } catch (err) {
+      const { statusCode, payload } = internalServerError(
+        "DELETE_HELPER_ERROR",
+        err.message
+      );
+      res.status(statusCode).json(payload);
+    }
+  }
+
+  async editHelper(req, res) {
+    try {
+      const { id } = req.params;
+      const tokenId = req.user.id;
+      const { title, headerBackgroundColor, linkFontColor, iconColor, userId } =
+        req.body;
+
+      if (!title) {
+        return res.status(400).json({
+          errors: [{ msg: "title is required" }],
+        });
+      }
+
+      if (userId !== tokenId) {
+        return res.status(401).json({
+          errors: [{ msg: "User not authorized to edit this helper" }],
+        });
+      }
+
+      if (
+        (headerBackgroundColor && !validateHexColor(headerBackgroundColor)) ||
+        (linkFontColor && !validateHexColor(linkFontColor)) ||
+        (iconColor && !validateHexColor(iconColor))
+      ) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: "Invalid value for headerBackgroundColor, linkFontColor or iconColor",
+            },
+          ],
+        });
+      }
+
+      const updatedHelper = await helperService.updateHelper(id, req.body);
+      res.status(200).json(updatedHelper);
+    } catch (err) {
+      const { statusCode, payload } = internalServerError(
+        "EDIT_HELPER_ERROR",
+        err.message
+      );
+      res.status(statusCode).json(payload);
+    }
+  }
+
+  async getAllHelpers(_, res) {
+    try {
+      const links = await helperService.getAllHelpers();
+      res.status(200).json(links);
+    } catch (err) {
+      const { statusCode, payload } = internalServerError(
+        "GET_ALL_HELPERS_ERROR",
+        err.message
+      );
+      res.status(statusCode).json(payload);
+    }
+  }
+
+  async getHelpersByUserId(req, res) {
+    try {
+      const userId = req.user.id;
+      const helpers = await helperService.getHelpersByUserId(userId);
+      res.status(200).json(helpers);
+    } catch (err) {
+      const { statusCode, payload } = internalServerError(
+        "GET_HELPERS_ERROR",
+        err.message
+      );
+      res.status(statusCode).json(payload);
+    }
+  }
+
+  async getHelperById(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (isNaN(id) || id.trim() === "") {
+        return res.status(400).json({ errors: [{ msg: "Invalid helper ID" }] });
+      }
+
+      const helper = await helperService.getHelperById(id);
+
+      if (!helper) {
+        return res.status(404).json({ errors: [{ msg: "Helper not found" }] });
+      }
+
+      res.status(200).json(helper);
+    } catch (err) {
+      const { statusCode, payload } = internalServerError(
+        "GET_HELPER_BY_ID_ERROR",
+        err.message
+      );
+      res.status(statusCode).json(payload);
+    }
+  }
+}
+
+module.exports = new LinkController();
