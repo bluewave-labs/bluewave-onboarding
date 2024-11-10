32ae0f221e2d93993be9b71c2add6280f25ac1c9
d02ev <vikramadityapratapsingh98@gmail.com>
2024-11-10 23:24:16 +0530

    added login and signup routing based upon team existence

--- frontend/src/components/Private.jsx
+++ frontend/src/components/Private.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../services/authProvider';
+import { getTeamCount } from '../services/teamServices';
 import LoadingPage from './LoadingPage/LoadingPage'
 
 const Private = ({ Component }) => {
   const { isLoggedIn, isFetching } = useAuth();
+  const [teamExistence, setTeamExistence] = useState(false);
+  const [isFetchingTeam, setFetchingTeam] = useState(true);
 
-  return isFetching ? <LoadingPage /> : (isLoggedIn ? <Component /> : <Navigate to="/login" />);
+  useEffect(() => {
+    const fetchTeamCount = async () => {
+      try {
+        const { teamExists } = await getTeamCount();
+        setTeamExistence(teamExists);
+      } catch (err) {
+        console.error(err);
+        setTeamExistence(false);
+      } finally {
+        setFetchingTeam(false);
+      }
+    };
+    fetchTeamCount();
+  }, []);
+
+  return (isFetching || isFetchingTeam) ? <LoadingPage /> : (isLoggedIn ? <Component /> : (teamExistence ? <Navigate to="/login" /> : <Navigate to="/signup" />));
 }
 
 export default Private;
