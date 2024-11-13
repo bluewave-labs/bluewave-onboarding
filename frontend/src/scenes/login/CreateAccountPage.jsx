9456dd59a500965b438cce337e657c5b32415094
Amaan Pathan <eulerbutcooler@gmail.com>
2024-11-13 23:35:32 +0530

    Fixed onBlur validation checks

--- frontend/src/scenes/login/CreateAccountPage.jsx
+++ frontend/src/scenes/login/CreateAccountPage.jsx
@@ -10,7 +10,7 @@ import { useAuth } from "../../services/authProvider";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../components/Logo/Logo";
 import * as Yup from "yup";
-import { useFormik } from "formik";
+import { Form, Formik } from "formik";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
@@ -47,202 +47,177 @@ function CreateAccountPage() {
   const { loginAuth } = useAuth();
   const navigate = useNavigate();
   const [serverErrors, setServerErrors] = useState([]);
-
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      surname: "",
-      email: "",
-      password: "",
-    },
-    validationSchema,
-    validateOnMount: false,
-    onSubmit: async (values, { setSubmitting }) => {
-      setServerErrors([]);
-      try {
-        const response = await signUp(values);
-        handleAuthSuccess(response, loginAuth, navigate);
-      } catch (error) {
-        if (error.response?.data?.errors) {
-          setServerErrors(error.response.data.errors);
-        } else if (error.response?.data?.error) {
-          setServerErrors([error.response.data.error]);
-        } else {
-          setServerErrors([
-            "An error occurred. Please check your network connection and try again.",
-          ]);
-        }
-      } finally {
-        setSubmitting(false);
-      }
-    },
-  });
-
-  const isFormEmpty =
-    !formik.values.name &&
-    !formik.values.surname &&
-    !formik.values.email &&
-    !formik.values.password;
-
-  const hasMinLength = formik.values.password.length >= 8;
-  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>_\-=]/.test(
-    formik.values.password
-  );
-
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        formik.handleSubmit(e);
-        Object.keys(formik.values).forEach((key) => {
-          formik.setFieldTouched(key, true, false);
-        });
+    <Formik
+      initialValues={{
+        name: "",
+        surname: "",
+        email: "",
+        password: "",
       }}
-      className={styles["login-container"]}
+      validationSchema={validationSchema}
+      validateOnMount={false}
+      validateonBlur={true}
+      onSubmit={async (values, { setSubmitting }) => {
+        setServerErrors([]);
+        try {
+          const response = await signUp(values);
+          handleAuthSuccess(response, loginAuth, navigate);
+        } catch (error) {
+          if (error.response?.data?.errors) {
+            setServerErrors(error.response.data.errors);
+          } else if (error.response?.data?.error) {
+            setServerErrors([error.response.data.error]);
+          } else {
+            setServerErrors([
+              "An error occurred. Please check your network connection and try again.",
+            ]);
+          }
+        } finally {
+          setSubmitting(false);
+        }
+      }}
     >
-      <Logo />
-      <h2>Create an account</h2>
+      {({
+        isSubmitting,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        values,
+      }) => (
+        <Form className={styles["login-container"]}>
+          <Logo />
+          <h2>Create an account</h2>
 
-      <div className={styles["form-group"]}>
-        <CustomTextField
-          id="name"
-          name="name"
-          type="text"
-          labelText="Name*:"
-          checkCircleIconVisible={true}
-          displayCheckCircleIcon={formik.touched.name && !formik.errors.name}
-          placeholder="Enter your name"
-          textFieldMargin="none"
-          TextFieldWidth="full"
-          required={true}
-          value={formik.values.name}
-          onChange={(e) => {
-            formik.handleChange(e);
-          }}
-          onBlur={formik.handleBlur}
-          error={Boolean(formik.touched.name && formik.errors.name)}
-          helperText={formik.touched.name && formik.errors.name}
-        />
-      </div>
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="name"
+              name="name"
+              type="text"
+              labelText="Name*:"
+              checkCircleIconVisible={true}
+              displayCheckCircleIcon={touched.name && !errors.name}
+              placeholder="Enter your name"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.name && errors.name)}
+              helperText={touched.name && errors.name}
+            />
+          </div>
 
-      <div className={styles["form-group"]}>
-        <CustomTextField
-          id="surname"
-          name="surname"
-          type="text"
-          labelText="Surname*:"
-          checkCircleIconVisible={true}
-          displayCheckCircleIcon={
-            formik.touched.surname && !formik.errors.surname
-          }
-          placeholder="Enter your surname"
-          textFieldMargin="none"
-          TextFieldWidth="full"
-          required={true}
-          value={formik.values.surname}
-          onChange={(e) => {
-            formik.handleChange(e);
-            formik.setFieldTouched("name", true, false);
-          }}
-          onBlur={formik.handleBlur}
-          error={Boolean(formik.touched.surname && formik.errors.surname)}
-          helperText={formik.touched.surname && formik.errors.surname}
-        />
-      </div>
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="surname"
+              name="surname"
+              type="text"
+              labelText="Surname*:"
+              checkCircleIconVisible={true}
+              displayCheckCircleIcon={touched.surname && !errors.surname}
+              placeholder="Enter your surname"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.surname}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.surname && errors.surname)}
+              helperText={touched.surname && errors.surname}
+            />
+          </div>
 
-      <div className={styles["form-group"]}>
-        <CustomTextField
-          id="email"
-          type="email"
-          name="email"
-          labelText="Email*:"
-          checkCircleIconVisible={true}
-          displayCheckCircleIcon={formik.touched.email && !formik.errors.email}
-          placeholder="Enter your email"
-          textFieldMargin="none"
-          TextFieldWidth="full"
-          required={true}
-          value={formik.values.email}
-          onChange={(e) => {
-            formik.handleChange(e);
-            formik.setFieldTouched("surname", true, false);
-          }}
-          onBlur={formik.handleBlur}
-          error={Boolean(formik.touched.email && formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-      </div>
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="email"
+              type="email"
+              name="email"
+              labelText="Email*:"
+              checkCircleIconVisible={true}
+              displayCheckCircleIcon={touched.email && !errors.email}
+              placeholder="Enter your email"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.email && errors.email)}
+              helperText={touched.email && errors.email}
+            />
+          </div>
 
-      <div className={styles["form-group"]}>
-        <CustomTextField
-          id="password"
-          type="password"
-          name="password"
-          labelText="Password*:"
-          checkCircleIconVisible={true}
-          displayCheckCircleIcon={
-            formik.touched.password && !formik.errors.password
-          }
-          placeholder="Create your password"
-          textFieldMargin="none"
-          TextFieldWidth="full"
-          required={true}
-          value={formik.values.password}
-          onChange={(e) => {
-            formik.handleChange(e);
-            formik.setFieldTouched("email", true, false);
-            formik.setFieldTouched("password", true, false);
-          }}
-          onBlur={formik.handleBlur}
-          error={Boolean(formik.touched.password && formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-      </div>
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="password"
+              type="password"
+              name="password"
+              labelText="Password*:"
+              checkCircleIconVisible={true}
+              displayCheckCircleIcon={touched.password && !errors.password}
+              placeholder="Create your password"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.password && errors.password)}
+              helperText={touched.password && errors.password}
+            />
+          </div>
 
-      <div className={styles["password-constraints"]}>
-        <CheckCircleIcon
-          style={{
-            color: hasMinLength ? "green" : "#D0D5DD",
-            fontSize: "20px",
-            marginRight: "5px",
-          }}
-        />
-        Must be at least 8 characters
-      </div>
-      <div className={styles["password-constraints"]}>
-        <CheckCircleIcon
-          style={{
-            color: hasSpecialChar ? "green" : "#D0D5DD",
-            fontSize: "20px",
-            marginRight: "5px",
-          }}
-        />
-        Must contain one special character
-      </div>
+          <div className={styles["password-constraints"]}>
+            <CheckCircleIcon
+              style={{
+                color: values.password.length >= 8 ? "green" : "#D0D5DD",
+                fontSize: "20px",
+                marginRight: "5px",
+              }}
+            />
+            Must be at least 8 characters
+          </div>
+          <div className={styles["password-constraints"]}>
+            <CheckCircleIcon
+              style={{
+                color: /[!@#$%^&*(),.?":{}|<>_\-=]/.test(values.password)
+                  ? "green"
+                  : "#D0D5DD",
+                fontSize: "20px",
+                marginRight: "5px",
+              }}
+            />
+            Must contain one special character
+          </div>
 
-      {serverErrors.length > 0 && (
-        <div className={styles["error-message"]}>
-          {serverErrors.map((error, index) => (
-            <div key={index}>{error}</div>
-          ))}
-        </div>
+          {serverErrors.length > 0 && (
+            <div className={styles["error-message"]}>
+              {serverErrors.map((error, index) => (
+                <div key={index}>{error}</div>
+              ))}
+            </div>
+          )}
+
+          <button
+            className={styles["create-account-button"]}
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? (
+              <CircularProgress size={12} color="inherit" />
+            ) : (
+              "Get started"
+            )}
+          </button>
+          <div className={styles["sign-up-link"]}>
+            Already have an account? <CustomLink text="Log in" url="/" />
+          </div>
+        </Form>
       )}
-
-      <button
-        className={styles["create-account-button"]}
-        type="submit"
-        disabled={isFormEmpty || formik.isSubmitting}
-      >
-        {formik.isSubmitting ? (
-          <CircularProgress size={12} color="inherit" />
-        ) : (
-          "Get started"
-        )}
-      </button>
-      <div className={styles["sign-up-link"]}>
-        Already have an account? <CustomLink text="Log in" url="/" />
-      </div>
-    </form>
+    </Formik>
   );
 }
 
--- frontend/src/scenes/login/LoginPage.jsx
+++ frontend/src/scenes/login/LoginPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import styles from "./Login.module.css";
 import CustomTextField from "../../components/TextFieldComponents/CustomTextField/CustomTextField";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -8,8 +10,6 @@ import { handleAuthSuccess } from "../../utils/loginHelper";
 import { useAuth } from "../../services/authProvider";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../components/Logo/Logo";
-import * as Yup from "yup";
-import { useFormik } from "formik";
 
 const validationSchema = Yup.object({
   email: Yup.string()
@@ -24,125 +24,122 @@ const validationSchema = Yup.object({
 
 function LoginPage() {
   const [rememberMe, setRememberMe] = useState(false);
+  const [serverErrors, setServerErrors] = useState([]);
   const { loginAuth } = useAuth();
   const navigate = useNavigate();
-  const [serverErrors, setServerErrors] = useState([]);
-
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationSchema,
-    validateOnMount: false,
-    onSubmit: async (values, { setSubmitting }) => {
-      setServerErrors([]);
-      try {
-        const response = await login(values);
-        handleAuthSuccess(response, loginAuth, navigate);
-      } catch (error) {
-        if (error.response?.data?.errors) {
-          setServerErrors(error.response.data.errors);
-        } else if (error.response?.data?.error) {
-          setServerErrors([error.response.data.error]);
-        } else {
-          setServerErrors([
-            "An error occurred. Please check your network connection and try again.",
-          ]);
-        }
-      } finally {
-        setSubmitting(false);
-      }
-    },
-  });
-
-  const isFormEmpty = !formik.values.email && !formik.values.password;
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        formik.handleSubmit(e);
-        Object.keys(formik.values).forEach((key) => {
-          formik.setFieldTouched(key, true, false);
-        });
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
       }}
-      className={styles["login-container"]}
+      validationSchema={validationSchema}
+      validateOnChange={false}
+      validateOnBlur={true}
+      onSubmit={async (values, { setSubmitting }) => {
+        setServerErrors([]);
+        try {
+          const response = await login(values);
+          handleAuthSuccess(response, loginAuth, navigate);
+        } catch (error) {
+          if (error.response?.data?.errors) {
+            setServerErrors(error.response.data.errors);
+          } else if (error.response?.data?.error) {
+            setServerErrors([error.response.data.error]);
+          } else {
+            setServerErrors([
+              "An error occurred. Please check your network connection and try again.",
+            ]);
+          }
+        } finally {
+          setSubmitting(false);
+        }
+      }}
     >
-      <Logo />
-      <h2>Log in to your account</h2>
-      <div className={styles["form-group"]}>
-        <CustomTextField
-          id="email"
-          placeholder="Enter email"
-          labelText="Email:"
-          textFieldMargin="none"
-          TextFieldWidth="full"
-          required={true}
-          value={formik.values.email}
-          onChange={(e) => {
-            formik.handleChange(e);
-          }}
-          onBlur={formik.handleBlur}
-          error={Boolean(formik.touched.email && formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-      </div>
-      <div className={styles["form-group"]}>
-        <CustomTextField
-          id="password"
-          placeholder="Enter password"
-          labelText="Password:"
-          textFieldMargin="none"
-          TextFieldWidth="full"
-          type="password"
-          required={true}
-          value={formik.values.password}
-          onChange={(e) => {
-            formik.handleChange(e);
-            formik.setFieldTouched("email", true, false);
-            formik.setFieldTouched("password", true, false);
-          }}
-          onBlur={formik.handleBlur}
-          error={Boolean(formik.touched.password && formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
-        />
-        {serverErrors.length > 0 && (
-          <div className={styles["error-message"]}>
-            {serverErrors.map((error, index) => (
-              <div key={index}>{error}</div>
-            ))}
-          </div>
-        )}
-      </div>
-      <div className={styles["form-group"]}>
-        <div className={styles["form-group-2"]}>
-          <label>
-            <input
-              type="checkbox"
-              checked={rememberMe}
-              onChange={(e) => setRememberMe(e.target.checked)}
+      {({
+        isSubmitting,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        values,
+      }) => (
+        <Form className={styles["login-container"]}>
+          <Logo />
+          <h2>Log in to your account</h2>
+
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="email"
+              name="email"
+              placeholder="Enter email"
+              labelText="Email:"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.email && errors.email)}
+              helperText={touched.email && errors.email}
             />
-            Remember for 30 days
-          </label>
-          <CustomLink text="Forgot Password" url="/forgot-password" />
-        </div>
-      </div>
-      <button
-        className={styles["sign-in-button"]}
-        type="submit"
-        disabled={isFormEmpty || formik.isSubmitting}
-      >
-        {formik.isSubmitting ? (
-          <CircularProgress size={12} color="inherit" />
-        ) : (
-          "Sign In"
-        )}
-      </button>
-      <div className={styles["sign-up-link"]}>
-        Don't have an account? <CustomLink text="Sign up" url="/signup" />
-      </div>
-    </form>
+          </div>
+
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter password"
+              labelText="Password:"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.password && errors.password)}
+              helperText={touched.password && errors.password}
+            />
+
+            {serverErrors.length > 0 && (
+              <div className={styles["error-message"]}>
+                {serverErrors.map((error, index) => (
+                  <div key={index}>{error}</div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <div className={styles["form-group"]}>
+            <div className={styles["form-group-2"]}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={rememberMe}
+                  onChange={(e) => setRememberMe(e.target.checked)}
+                />
+                Remember for 30 days
+              </label>
+              <CustomLink text="Forgot Password" url="/forgot-password" />
+            </div>
+          </div>
+
+          <button
+            className={styles["sign-in-button"]}
+            type="submit"
+            disabled={!values.email || !values.password || isSubmitting}
+          >
+            {isSubmitting ? (
+              <CircularProgress size={12} color="inherit" />
+            ) : (
+              "Sign In"
+            )}
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
