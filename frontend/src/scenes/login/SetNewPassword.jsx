662fad06faf9e17831cc9fc9f7911c0f4f7487bb
Amaan Pathan <eulerbutcooler@gmail.com>
2024-11-14 12:00:12 +0530

    Enhanced login page with sign-up link, improved validation for name and surname fields and used Formik and Yup to simplify SetNewPassword

--- frontend/src/scenes/login/SetNewPassword.jsx
+++ frontend/src/scenes/login/SetNewPassword.jsx
@@ -1,121 +1,169 @@
-import React, { useState } from 'react';
-import styles from './Login.module.css';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CustomTextField from '@components/TextFieldComponents/CustomTextField/CustomTextField';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import CircularProgress from '@mui/material/CircularProgress';
-import { resetPassword } from '../../services/loginServices';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CustomTextField from "@components/TextFieldComponents/CustomTextField/CustomTextField";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CircularProgress from "@mui/material/CircularProgress";
+import { resetPassword } from "../../services/loginServices";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik } from "formik";
 
-function SetNewPasswordPage({ email = 'asdf@asdf.com' }) {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [isPasswordValid, setIsPasswordValid] = useState(false);
-  const [hasSpecialCharacter, setHasSpecialCharacter] = useState(false);
-  const [atLeastEightCharacters, setAtLeastEightCharacters] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+function SetNewPasswordPage({ email = "asdf@asdf.com" }) {
+  const [serverErrors, setServerErrors] = useState([]);
   const navigate = useNavigate();
 
-  const handleResetPassword = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setError('');
-    try {
-      const response = await resetPassword({ email, password });
-      setLoading(false);
-      console.log('Password Reset successful:', response);
-      navigate('/reset-password');
-    } catch (error) {
-      setLoading(false);
-      console.error('Password Reset failed:', error);
-      if (error.response?.data?.error) {
-        setError(error.response.data.error);
-      }
-      else {
-        setError('An error occurred. Please try again.');
-      }
-    }
-  };
-
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    setHasSpecialCharacter(hasSpecialCharacterCheck(e.target.value));
-    setAtLeastEightCharacters(atLeastEightCharactersCheck(e.target.value));
-    setIsPasswordValid(validatePassword(e.target.value));
-  };
-
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  };
-
-  const hasSpecialCharacterCheck = (password) => {
-    const regex = /[!@#$%^&*-_]/;
-    return regex.test(password);
-  };
-
-  const atLeastEightCharactersCheck = (password) => {
-    return password.length >= 8;
-  };
-
-  const validatePassword = (password) => {
-    return atLeastEightCharactersCheck(password) && hasSpecialCharacterCheck(password);
-  };
+  const validationSchema = Yup.object({
+    password: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(
+        /[!@#$%^&*(),.?":{}|<>_\-=]/,
+        "Password must contain at least one special character"
+      ),
+    confirmPassword: Yup.string()
+      .required("Password is required")
+      .min(8, "Password must be at least 8 characters")
+      .matches(
+        /[!@#$%^&*(),.?":{}|<>_\-=]/,
+        "Password must contain at least one special character"
+      ),
+  });
 
   return (
-    <div className={styles["login-body"]}>
-      <form onSubmit={handleResetPassword} className={styles["login-container"]}>
-        <h2 style={{ marginBottom: "0px" }}>Set new Password</h2>
-        <h3>Your new password must be different to previously used passwords.</h3>
-        <div className={styles["form-group"]}>
-          <CustomTextField
-            id="password"
-            type="password"
-            name="password"
-            labelText='Password*:'
-            checkCircleIconVisible={true}
-            displayCheckCircleIcon={isPasswordValid}
-            placeholder='Create your password'
-            textFieldMargin='none'
-            TextFieldWidth="full"
-            required={true}
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div className={styles["form-group"]} style={{ marginBottom: 0 }}>
-          <CustomTextField
-            id="confirmPassword"
-            type="password"
-            name="confirmPassword"
-            labelText='Confirm Password*:'
-            checkCircleIconVisible={true}
-            displayCheckCircleIcon={isPasswordValid && password === confirmPassword}
-            placeholder='Confirm your password'
-            textFieldMargin='none'
-            TextFieldWidth="full"
-            required={true}
-            value={confirmPassword}
-            onChange={handleConfirmPasswordChange}
-          />
-          {error && <div className={styles["error-message"]}>{error}</div>}
-        </div>
+    <Formik
+      initialValues={{
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={validationSchema}
+      validateOnChange={false}
+      validateOnBlur={true}
+      onSubmit={async (values, { setSubmitting }) => {
+        setServerError([]);
+        try {
+          const respone = await resetPassword(email, values.password);
+          navigate("/reset-password");
+        } catch (error) {
+          console.error("Password Reset failed:", error);
+          if (error.respone?.data?.errors) {
+            setServerErrors(error.respone?.data?.errors);
+          } else if (error.response?.data?.error) {
+            setServerErrors([error.response.data.error]);
+          } else {
+            setServerErrors(["An error occurred. Please try again."]);
+          }
+        } finally {
+          setSubmitting(false);
+        }
+      }}
+    >
+      {({
+        isSubmitting,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        values,
+      }) => (
+        <div className={styles["login-body"]}>
+          <Form className={styles["login-container"]}>
+            <h2 style={{ marginBottom: "0px" }}>Set new Password</h2>
+            <h3>
+              Your new password must be different to previously used passwords.
+            </h3>
+            <div className={styles["form-group"]}>
+              <CustomTextField
+                id="password"
+                type="password"
+                name="password"
+                labelText="Password*:"
+                checkCircleIconVisible={true}
+                displayCheckCircleIcon={touched.password && !errors.password}
+                placeholder="Create your password"
+                textFieldMargin="none"
+                TextFieldWidth="full"
+                required={true}
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </div>
+            <div className={styles["form-group"]} style={{ marginBottom: 0 }}>
+              <CustomTextField
+                id="confirmPassword"
+                type="password"
+                name="confirmPassword"
+                labelText="Confirm Password*:"
+                checkCircleIconVisible={true}
+                displayCheckCircleIcon={
+                  touched.confirmPassword &&
+                  errors.confirmPassword &&
+                  values.password === values.confirmPassword
+                }
+                placeholder="Confirm your password"
+                textFieldMargin="none"
+                TextFieldWidth="full"
+                required={true}
+                value={values.confirmPassword}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              {serverErrors && (
+                <div className={styles["error-message"]}>{serverErrors}</div>
+              )}
+            </div>
 
-        <div className={styles["password-constraints"]}>
-          <CheckCircleIcon style={{ color: atLeastEightCharacters ? 'green' : 'var(--light-border-color)', fontSize: '20px', marginRight: "5px" }} />
-          Must be at least 8 characters
+            <div className={styles["password-constraints"]}>
+              <CheckCircleIcon
+                style={{
+                  color:
+                    values.password >= 8
+                      ? "green"
+                      : "var(--light-border-color)",
+                  fontSize: "20px",
+                  marginRight: "5px",
+                }}
+              />
+              Must be at least 8 characters
+            </div>
+            <div className={styles["password-constraints"]}>
+              <CheckCircleIcon
+                style={{
+                  color: /[!@#$%^&*(),.?":{}|<>_\-=]/.test(values.password)
+                    ? "green"
+                    : "var(--light-border-color)",
+                  fontSize: "20px",
+                  marginRight: "5px",
+                }}
+              />
+              Must contain one special character
+            </div>
+            <button
+              type="submit"
+              className={styles["sign-in-button"]}
+              style={{ marginTop: "15px" }}
+              disabled={loading}
+            >
+              {loading ? (
+                <CircularProgress size={12} color="inherit" />
+              ) : (
+                "Reset Password"
+              )}
+            </button>
+            <button
+              type="button"
+              onClick={() => navigate("/login")}
+              className={styles["back-to-login-button"]}
+            >
+              {" "}
+              <ArrowBackIcon style={{ fontSize: "18px", marginRight: "5px" }} />
+              Back to log in
+            </button>
+          </Form>
         </div>
-        <div className={styles["password-constraints"]}>
-          <CheckCircleIcon style={{ color: hasSpecialCharacter ? 'green' : 'var(--light-border-color)', fontSize: '20px', marginRight: "5px" }} />
-          Must contain one special character
-        </div>
-        <button type="submit" className={styles["sign-in-button"]} style={{ marginTop: "15px" }} disabled={loading}>
-          {loading ? <CircularProgress size={12} color="inherit" /> : 'Reset Password'}
-        </button>
-        <button type="button" onClick={() => navigate('/login')} className={styles["back-to-login-button"]}> <ArrowBackIcon style={{ fontSize: "18px", marginRight: "5px" }} />Back to log in</button>
-      </form >
-    </div >
+      )}
+    </Formik>
   );
 }
 
