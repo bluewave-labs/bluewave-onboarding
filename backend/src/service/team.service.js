fcdd2a30d281825087f531f474220abee3934106
Debora Serra <debora.r.serra@gmail.com>
2024-11-22 14:13:18 -0800

    test: add unit tests to team service

--- backend/src/service/team.service.js
+++ backend/src/service/team.service.js
@@ -15,6 +15,7 @@ class TeamService {
             await transaction.commit();
             return team;
         } catch (err) {
+            await transaction.rollback();
             throw new Error("Failed to create team");
         }
     }
@@ -124,7 +125,7 @@ class TeamService {
         }
         catch (err) {
             await transaction.rollback();
-            throw new Error(`"Failed to update user role ~ ${err.message}`);
+            throw new Error(`Failed to update user role ~ ${err.message}`);
         }
     }
 }
