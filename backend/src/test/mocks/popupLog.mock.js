cb090916c3090b19731d5464837b09f225abae42
Debora Serra <debora.r.serra@gmail.com>
2024-11-27 12:33:57 -0800

    test: add e2e tests to popup log

--- backend/src/test/mocks/popupLog.mock.js
+++ backend/src/test/mocks/popupLog.mock.js
@@ -1,15 +1,40 @@
 class PopupLogBuilder {
   constructor() {
     this.popupLog = {
-      popupType: 'guide',
-      userId: 1,
+      popupType: "guide",
+      userId: "1",
       showingTime: new Date(),
       completed: false,
-  }
+    };
   }
 
   static popupLog() {
     return new PopupLogBuilder();
+  }
+
+  invalidPopupType() {
+    this.popupLog.popupType = "invalid";
+    return this;
+  }
+
+  missingPopupType() {
+    delete this.popupLog.popupType;
+    return this;
+  }
+
+  invalidUserId() {
+    this.popupLog.userId = 123;
+    return this;
+  }
+
+  missingUserId() {
+    delete this.popupLog.userId;
+    return this;
+  }
+
+  invalidCompleted() {
+    this.popupLog.completed = "invalid";
+    return this;
   }
 
   missingCompleted() {
@@ -22,9 +47,11 @@ class PopupLogBuilder {
   }
 }
 
-const popupLogList = Array.from({ length: 5 }, (_, i) => PopupLogBuilder.popupLog().build());
+const popupLogList = Array.from({ length: 5 }, (_, i) =>
+  PopupLogBuilder.popupLog().build()
+);
 
 module.exports = {
   PopupLogBuilder,
-  popupLogList
+  popupLogList,
 };
