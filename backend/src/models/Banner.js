a4ff78319bb840f21797c0980cee8b03a02a3cdf
Debora Serra <debora.r.serra@gmail.com>
2024-12-10 15:22:12 -0800

    feat: add column action url to models

--- backend/src/models/Banner.js
+++ backend/src/models/Banner.js
@@ -1,74 +1,90 @@
-const { validateHexColor, validateActionButton } = require('../utils/guide.helper');
-const { validatePositionWrapper } = require('../utils/banner.helper');
+const {
+  validateHexColor,
+  validateActionButton,
+} = require("../utils/guide.helper");
+const { validatePositionWrapper } = require("../utils/banner.helper");
 
 module.exports = (sequelize, DataTypes) => {
-    const Banner = sequelize.define('Banner', {
-        closeButtonAction: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              isValidAction(value) {
-                  validateActionButton(value);
-              },
-            },
+  const Banner = sequelize.define(
+    "Banner",
+    {
+      closeButtonAction: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isValidAction(value) {
+            validateActionButton(value);
           },
-          position: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-              isValidPosition(value) {
-                  validatePositionWrapper(value);
-              },
-            },
-          },
-          url: {
-            type: DataTypes.STRING,
-            allowNull: true,
-          },
-          fontColor: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: "#FFFFFF",
-            validate: {
-              isHexColor(value) {
-                  validateHexColor(value, 'fontColor');
-              },
-            },
-          },
-          backgroundColor: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: "#FFFFFF",
-            validate: {
-              isHexColor(value) {
-                  validateHexColor(value, 'backgroundColor');
-              },
-            },
-          },
-          bannerText: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: "",
-          },
-
-        createdBy: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: "users",
-                key: "id",
-            },
         },
+      },
+      position: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isValidPosition(value) {
+            validatePositionWrapper(value);
+          },
+        },
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      fontColor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "#FFFFFF",
+        validate: {
+          isHexColor(value) {
+            validateHexColor(value, "fontColor");
+          },
+        },
+      },
+      backgroundColor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "#FFFFFF",
+        validate: {
+          isHexColor(value) {
+            validateHexColor(value, "backgroundColor");
+          },
+        },
+      },
+      bannerText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "",
+      },
+      actionUrl: {
+        type: DataTypes.STRING,
+        validate: {
+          isUrl(value) {
+            try {
+              new URL(value);
+            } catch {
+              throw new Error("Invalid URL for actionUrl");
+            }
+          },
+        },
+      },
+
+      createdBy: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
     },
-        {
-            tableName: "banners",
-            timestamps: false,
-        },
-    );
+    {
+      tableName: "banners",
+      timestamps: false,
+    }
+  );
 
-    Banner.associate = (models) => {
-        Banner.belongsTo(models.User, { foreignKey: "createdBy", as: "creator" });
-    };
-    return Banner;
+  Banner.associate = (models) => {
+    Banner.belongsTo(models.User, { foreignKey: "createdBy", as: "creator" });
+  };
+  return Banner;
 };
-
