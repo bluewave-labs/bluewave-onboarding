961486582d28f696639ee4de3697b3e96125e6c0
Aryan Pokharkar <aryan.pokharkar21@vit.edu>
2024-12-13 22:03:14 +0530

    Fixed missing import

--- backend/migrations/20241022190720-create-tours-table.js
+++ backend/migrations/20241022190720-create-tours-table.js
@@ -1,3 +1,4 @@
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.sequilize.transaction(async (transaction) => {
