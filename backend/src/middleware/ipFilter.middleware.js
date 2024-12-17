91c37d7c9a5c54906f91112079b3be20974fefb7
Debora Serra <debora.r.serra@gmail.com>
2024-12-17 07:56:08 -0800

    feat: create ip filter middleware

---
+++ backend/src/middleware/ipFilter.middleware.js
@@ -0,0 +1,50 @@
+const parseIpRange = (allowedIpsEnv) => {
+  if (!allowedIpsEnv) return [];
+
+  const ranges = allowedIpsEnv.split(",").map((range) => range.trim());
+  // IP range expected format is "baseIp/rangeStart-rangeEnd" (e.g. 192.168.1/1-255)
+  return ranges.map((range) => {
+    const [baseIp, endRange] = range.split("/");
+    if (!baseIp || !endRange) throw new Error("Invalid IP range format");
+    const rangeParts = endRange.split("-");
+    return {
+      baseIp,
+      rangeStart: parseInt(rangeParts[0]),
+      rangeEnd: parseInt(rangeParts[1]),
+    };
+  });
+};
+
+const isIpAllowed = (currentIp, allowedRanges) => {
+  const ipParts = currentIp.split(".");
+  const startIp = ipParts.slice(0, 3).join(".");
+  const lastOctet = parseInt(ipParts[3], 10);
+
+  for (const range of allowedRanges) {
+    if (
+      startIp === range.baseIp &&
+      lastOctet >= range.rangeStart &&
+      lastOctet <= range.rangeEnd
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const ipFilter = async (req, res, next) => {
+  try {
+    const currentIp =
+      req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    const allowedRanges = parseIpRange(process.env.ALLOWED_IPS_RANGE);
+
+    if (!isIpAllowed(currentIp, allowedRanges)) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    next();
+  } catch (err) {
+    console.error("IP Filter Error:", err.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
