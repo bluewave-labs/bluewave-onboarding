d71f0a6d8ffc5c0cf9dd4e8c190a8aeba6fca6f5
Debora Serra <debora.r.serra@gmail.com>
2024-12-19 10:17:23 -0800

    feat: add statistics controller

---
+++ backend/src/controllers/statistics.controller.js
@@ -0,0 +1,15 @@
+const statisticsService = require("../service/statistics.service");
+
+class StatisticsController {
+  async getStatistics(req, res) {
+    try {
+      const userId = req.user.id;
+      const statistics = await statisticsService.generateStatistics({ userId });
+      res.status(200).json(statistics);
+    } catch {
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+}
+
+module.exports = new StatisticsController();
