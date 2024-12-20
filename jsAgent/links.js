abd2ed48465c095b0cb3288f3e81c28ba7f12a83
tunckiral <tunc.kiral@gmail.com>
2024-12-20 16:27:48 -0500

    adding footer

--- jsAgent/links.js
+++ jsAgent/links.js
@@ -13,51 +13,74 @@ const linksDefaultOptions = {
 
 bw.links={
     init:function(){
-        bw.links.putIcon();
-        bw.links.putPlaceHolder();
+        bw.links.putHtml();
+        bw.links.putHeader();
+       // bw.links.putPlaceHolder();
         bw.links.bindClick();
     },
-    putIcon:function(){
-        let temp_html = `
-        <div id="bw-links" style="position: fixed; bottom: 20px; right: 30px; z-index: 99; border: none; outline: none; cursor: pointer; display: flex; flex-direction: column; align-items: flex-end; justify-content: space-evenly; flex-wrap: nowrap; ">
-            <svg id="bw-link-icon" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M24 28V21M24 14H24.02M19.8 38.4L22.72 42.2933C23.1542 42.8723 23.3714 43.1618 23.6375 43.2653C23.8707 43.356 24.1293 43.356 24.3625 43.2653C24.6286 43.1618 24.8458 42.8723 25.28 42.2933L28.2 38.4C28.7863 37.6183 29.0794 37.2274 29.437 36.929C29.9138 36.5311 30.4766 36.2497 31.081 36.107C31.5343 36 32.0228 36 33 36C35.7956 36 37.1935 36 38.2961 35.5433C39.7663 34.9343 40.9343 33.7663 41.5433 32.2961C42 31.1935 42 29.7956 42 27V15.6C42 12.2397 42 10.5595 41.346 9.27606C40.7708 8.14708 39.8529 7.2292 38.7239 6.65396C37.4405 6 35.7603 6 32.4 6H15.6C12.2397 6 10.5595 6 9.27606 6.65396C8.14708 7.2292 7.2292 8.14708 6.65396 9.27606C6 10.5595 6 12.2397 6 15.6V27C6 29.7956 6 31.1935 6.45672 32.2961C7.06569 33.7663 8.23373 34.9343 9.7039 35.5433C10.8065 36 12.2044 36 15 36C15.9772 36 16.4657 36 16.919 36.107C17.5234 36.2497 18.0862 36.5311 18.563 36.929C18.9206 37.2274 19.2137 37.6183 19.8 38.4Z" stroke="#7F56D9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-        </div>`;
-        document.body.insertAdjacentHTML('beforeend', temp_html);
-    },
-    putPlaceHolder: function(){
+    putHtml:function(){
         const options = window.bwonboarddata.helperLink[0];
         let option = {
             ...linksDefaultOptions,
             ...options
         };
-
-        const temp_html = `<div id="bw-links-placeholder" style=" flex-direction: column; justify-content: space-between; display:none; position: width: 330px;  fixed; bottom: 70px; right: 30px; z-index: 99; border: none; outline: none; cursor: pointer;">
-
-            ${bw.links.generateHeader(option.headerBackgroundColor)}
         
+        let temp_html = `
+        <div style="position: fixed; bottom: 20px; right: 30px; z-index: 99;">
+            <div id="bw-links" style=" box-shadow: 0px 8px 8px -4px rgba(16, 24, 40, 0.031372549), 0px 20px 24px -4px rgba(16, 24, 40, 0.0784313725); width: 330px; display: flex; flex-direction: column; justify-content: space-between; ">
+                ${bw.links.putHeader(option.title, option.headerBackgroundColor)}
+                <div id="bw-links-content" style="margin: 0;padding: 26px 34px 44px; ">CONTENT</div>
+                ${bw.links.putFooter()}
+            </div>
+            <div style="display: flex; justify-content: flex-end;" >
+                <svg id="bw-link-icon" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24 28V21M24 14H24.02M19.8 38.4L22.72 42.2933C23.1542 42.8723 23.3714 43.1618 23.6375 43.2653C23.8707 43.356 24.1293 43.356 24.3625 43.2653C24.6286 43.1618 24.8458 42.8723 25.28 42.2933L28.2 38.4C28.7863 37.6183 29.0794 37.2274 29.437 36.929C29.9138 36.5311 30.4766 36.2497 31.081 36.107C31.5343 36 32.0228 36 33 36C35.7956 36 37.1935 36 38.2961 35.5433C39.7663 34.9343 40.9343 33.7663 41.5433 32.2961C42 31.1935 42 29.7956 42 27V15.6C42 12.2397 42 10.5595 41.346 9.27606C40.7708 8.14708 39.8529 7.2292 38.7239 6.65396C37.4405 6 35.7603 6 32.4 6H15.6C12.2397 6 10.5595 6 9.27606 6.65396C8.14708 7.2292 7.2292 8.14708 6.65396 9.27606C6 10.5595 6 12.2397 6 15.6V27C6 29.7956 6 31.1935 6.45672 32.2961C7.06569 33.7663 8.23373 34.9343 9.7039 35.5433C10.8065 36 12.2044 36 15 36C15.9772 36 16.4657 36 16.919 36.107C17.5234 36.2497 18.0862 36.5311 18.563 36.929C18.9206 37.2274 19.2137 37.6183 19.8 38.4Z" stroke="#7F56D9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </div>
+            
         </div>`;
-      
-        document.getElementById('bw-link-icon').insertAdjacentHTML('beforebegin', temp_html);
+        document.body.insertAdjacentHTML('beforeend', temp_html);
     },
+    putHeader: function(title, headerBackgroundColor){
 
-    generateHeader: function(bgColor){
-        const temp_header_html =`<div style="background-color:${bgColor}"> <div>`;
+        const temp_header_html =`
+        
+            <div id="bw-links-header" style="padding: 18px 30px; background-color:${headerBackgroundColor}; display: flex;     justify-content: space-around; align-items: center;">
+            <svg style="height: 24px; width: 24px; color: rgb(102, 112, 133); font-size: 24px;">
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m7.46 7.12-2.78 1.15c-.51-1.36-1.58-2.44-2.95-2.94l1.15-2.78c2.1.8 3.77 2.47 4.58 4.57M12 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3M9.13 4.54l1.17 2.78c-1.38.5-2.47 1.59-2.98 2.97L4.54 9.13c.81-2.11 2.48-3.78 4.59-4.59M4.54 14.87l2.78-1.15c.51 1.38 1.59 2.46 2.97 2.96l-1.17 2.78c-2.1-.81-3.77-2.48-4.58-4.59m10.34 4.59-1.15-2.78c1.37-.51 2.45-1.59 2.95-2.97l2.78 1.17c-.81 2.1-2.48 3.77-4.58 4.58">
+                </path>
+            </svg> 
+                <span style="margin:auto; flex-grow: 1; margin: 0 12px; font-family: Inter; font-size: 20px; font-weight: 600; line-height: 30px;">
+                ${title}
+                </span>
+                    <svg id="bw-links-close" viewBox="0 0 24 24" style="height: 20px; width: 20px; fill: rgb(152, 162, 179); font-size: 20px;">
+                <path d="M18.3 5.71a.996.996 0 0 0-1.41 0L12 10.59 7.11 5.7a.996.996 0 0 0-1.41 0c-.39.39-.39 1.02 0 1.41L10.59 12 5.7 16.89c-.39.39-.39 1.02 0 1.41s1.02.39 1.41 0L12 13.41l4.89 4.89c.39.39 1.02.39 1.41 0s.39-1.02 0-1.41L13.41 12l4.89-4.89c.38-.38.38-1.02 0-1.4">
+                </path>
+            </svg>
+            </div>
+        
+        `;
+        return temp_header_html;
+    },
+    putFooter: function(){
+        return '<p style="padding: 14px 0 11px;border-top: 1px solid #ebebeb; font-family: Inter; font-size: 0.688rem; font-weight: 400; line-height: 2.12; text-align: center;">Powered by BlueWave Onboarding</p>';
     },
     bindClick : function(){
-        bw.util.bindLive("#bw-links", "click", function(){
+        bw.util.bindLive("#bw-link-icon", "click", function(){
             bw.links.toggle();
-        })
+        });
+        bw.util.bindLive("#bw-links-close","click", function(){
+            document.getElementById('bw-links').style.display = 'none';    
+        });
     },
     show : function(){
-        document.getElementById('bw-links-placeholder').style.display = 'flex';
+        document.getElementById('bw-links').style.display = 'flex';
     },
     hide : function(){
-        document.getElementById('bw-links-placeholder').style.display = 'none';
+        document.getElementById('bw-links').style.display = 'none';
     },
     toggle: function(){
-        document.getElementById('bw-links-placeholder').style.display = document.getElementById('bw-links-placeholder').style.display=='none'? 'block':'none';
+        document.getElementById('bw-links').style.display = document.getElementById('bw-links').style.display=='none'? 'block':'none';
     }
 
 };
