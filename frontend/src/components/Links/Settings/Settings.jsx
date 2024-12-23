b935977d3dda4a2f88aaa32500c3c92cafa6310a
Debora Serra <debora.r.serra@gmail.com>
2024-12-22 22:57:05 -0800

    chore: change style import name

--- frontend/src/components/Links/Settings/Settings.jsx
+++ frontend/src/components/Links/Settings/Settings.jsx
@@ -2,7 +2,7 @@ import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import { useContext, useEffect, useRef, useState } from "react";
 import { HelperLinkContext } from "../../../services/linksProvider";
 import Switch from "../../Switch/Switch";
-import s from "./Settings.module.scss";
+import style from "./Settings.module.scss";
 
 const defaultState = {
   title: "",
@@ -88,16 +88,16 @@ const Settings = () => {
 
   return (
     <form
-      className={s.settings}
+      className={style.settings}
       ref={settingsRef}
       onSubmit={handleClose}
       role='form'
       data-testid='settings-form'
     >
-      <div className={s.settings__header}>
-        <span className={s["settings__header--title"]}>Add new link</span>
-        <div className={s["settings__header--right"]}>
-          <span className={s["settings__header--info"]}>Auto-saved</span>
+      <div className={style.settings__header}>
+        <span className={style["settings__header--title"]}>Add new link</span>
+        <div className={style["settings__header--right"]}>
+          <span className={style["settings__header--info"]}>Auto-saved</span>
           <CloseOutlinedIcon
             onClick={handleClose}
             style={{ color: "#98A2B3", fontSize: "20px", cursor: "pointer" }}
@@ -105,17 +105,17 @@ const Settings = () => {
           />
         </div>
       </div>
-      <div className={s.settings__content}>
+      <div className={style.settings__content}>
         <input
           type='hidden'
           name='id'
           value={state.id}
           onChange={handleChange}
         />
-        <label htmlFor='title' className={s["settings__content--label"]}>
-          <span className={s["settings__content--text"]}>Title</span>
+        <label htmlFor='title' className={style["settings__content--label"]}>
+          <span className={style["settings__content--text"]}>Title</span>
           <input
-            className={s["settings__content--input"]}
+            className={style["settings__content--input"]}
             id='title'
             type='text'
             name='title'
@@ -123,19 +123,19 @@ const Settings = () => {
             value={state.title}
           />
         </label>
-        <label htmlFor='url' className={s["settings__content--label"]}>
-          <span className={s["settings__content--text"]}>
+        <label htmlFor='url' className={style["settings__content--label"]}>
+          <span className={style["settings__content--text"]}>
             URL to open (can be a relative URL)
           </span>
           <input
-            className={s["settings__content--input"]}
+            className={style["settings__content--input"]}
             id='url'
             type='text'
             name='url'
             onChange={handleChange}
             value={state.url}
           />
-          <small className={s["settings__content--obs"]}>
+          <small className={style["settings__content--obs"]}>
             You can use a URL that starts with https:// or an internal address
             that starts with / (slash) character.
           </small>
@@ -143,7 +143,7 @@ const Settings = () => {
 
         <label
           htmlFor='switch'
-          className={`${s["settings__content--label"]} ${s.last}`}
+          className={`${style["settings__content--label"]} ${style.last}`}
         >
           <Switch
             id='switch'
