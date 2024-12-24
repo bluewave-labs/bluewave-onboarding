0fe1dccc64a414b1c542c0a4da6e3574d249ab41
tunckiral <tunc.kiral@gmail.com>
2024-12-24 13:26:08 -0500

    adding link item

--- jsAgent/links.js
+++ jsAgent/links.js
@@ -9,7 +9,7 @@ const linksDefaultOptions = {
 };
 
 const global_content_html=`
-            <li style="display: flex; align-items: center; height:24px;">
+            <li class="bw-links-li" style="display: flex; align-items: center; height:24px;">
                 <svg viewBox="0 0 16 16" width="16" height="16" style="padding-right:16px" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <g clip-path="url(#clip0_601_3829)">
                         <path d="M6.6668 8.66666C6.9531 9.04942 7.31837 9.36612 7.73783 9.59529C8.1573 9.82446 8.62114 9.96074 9.0979 9.99489C9.57466 10.029 10.0532 9.96024 10.501 9.79319C10.9489 9.62613 11.3555 9.36471 11.6935 9.02666L13.6935 7.02666C14.3007 6.39799 14.6366 5.55598 14.629 4.68199C14.6215 3.808 14.2709 2.97196 13.6529 2.35394C13.0348 1.73591 12.1988 1.38535 11.3248 1.37775C10.4508 1.37016 9.60881 1.70614 8.98013 2.31333L7.83347 3.45333M9.33347 7.33333C9.04716 6.95058 8.68189 6.63388 8.26243 6.4047C7.84297 6.17553 7.37913 6.03925 6.90237 6.00511C6.4256 5.97096 5.94708 6.03975 5.49924 6.20681C5.0514 6.37387 4.64472 6.63528 4.3068 6.97333L2.3068 8.97333C1.69961 9.602 1.36363 10.444 1.37122 11.318C1.37881 12.192 1.72938 13.028 2.3474 13.6461C2.96543 14.2641 3.80147 14.6147 4.67546 14.6222C5.54945 14.6298 6.39146 14.2939 7.02013 13.6867L8.16013 12.5467" 
@@ -25,7 +25,6 @@ const global_content_html=`
                 <a href="{{link}}" target="_blank" style="color:{{linkFontColor}}; text-decoration: none; font-family: Inter; font-size: 1rem; font-weight: 400;">{{title}}</a>
             </li>
         `;
-
 
 bw.links={
     init:function(){
@@ -57,9 +56,7 @@ bw.links={
         document.body.insertAdjacentHTML('beforeend', temp_html);
     },
     putHeader: function(title, headerBackgroundColor){
-
         const temp_header_html =`
-        
             <div id="bw-links-header" style="padding: 18px 30px; background-color:${headerBackgroundColor}; display: flex;     justify-content: space-around; align-items: center;">
             <svg style="height: 24px; width: 24px; color: rgb(102, 112, 133); font-size: 24px;">
                 <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m7.46 7.12-2.78 1.15c-.51-1.36-1.58-2.44-2.95-2.94l1.15-2.78c2.1.8 3.77 2.47 4.58 4.57M12 15c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3M9.13 4.54l1.17 2.78c-1.38.5-2.47 1.59-2.98 2.97L4.54 9.13c.81-2.11 2.48-3.78 4.59-4.59M4.54 14.87l2.78-1.15c.51 1.38 1.59 2.46 2.97 2.96l-1.17 2.78c-2.1-.81-3.77-2.48-4.58-4.59m10.34 4.59-1.15-2.78c1.37-.51 2.45-1.59 2.95-2.97l2.78 1.17c-.81 2.1-2.48 3.77-4.58 4.58">
@@ -73,7 +70,6 @@ bw.links={
                 </path>
             </svg>
             </div>
-        
         `;
         return temp_header_html;
     },
@@ -106,20 +102,24 @@ bw.links={
         bw.util.bindLive("#bw-link-icon", "click", function(){
             bw.links.toggle();
         });
-        bw.util.bindLive("#bw-links-close","click", function(){
+        bw.util.bindLive("#bw-links-close", "click", function(){
             document.getElementById('bw-links').style.display = 'none';    
         });
-    },
-    show : function(){
-        document.getElementById('bw-links').style.display = 'flex';
-    },
-    hide : function(){
-        document.getElementById('bw-links').style.display = 'none';
+
+        document.querySelectorAll(".bw-links-li a").forEach(function(item){
+            item.addEventListener("mouseover", function(e){
+                e.target.style.textDecoration = 'underline';
+            });
+            item.addEventListener("mouseout", function(e){
+                e.target.style.textDecoration = 'none';
+            });
+        });
+        
     },
     toggle: function(){
-        document.getElementById('bw-links').style.display = document.getElementById('bw-links').style.display=='none'? 'block':'none';
+        const element = document.getElementById('bw-links');
+        element.style.display = element.style.display=='none' ? 'block':'none';
     }
-
 };
 
 (async function () {
