6a6722c7350480317923cebcb4aa8abea9906c61
tunckiral <tunc.kiral@gmail.com>
2024-12-24 20:14:24 -0500

    update

--- jsAgent/banner.js
+++ jsAgent/banner.js
@@ -1,14 +1,19 @@
 console.log('banner.js is here!');
 
-const bannerDefaultOptions = {
-    closeButtonAction: "no action",
-    position: "top",
-    url: "https://www.google.com",
-    fontColor: "#2548ff",
-    backgroundColor: "#80e0ff",
-    bannerText: "Mad cow is here",
-    actionUrl: "https://www.google.com"
-};
+let temp_banner_html = `
+<div class="bw-banner" style="position: fixed; top: 50px; z-index: 999999; height:50px; width:435px; background-color:{{backGroundColor}}; left: 50%; transform: translate(-50%, -50%);
+    line-height: 13px; font-weight: 400; display: flex; align-items: center; justify-content: space-between; padding: 0.7rem; border-radius: 5px; height:50px; width:435px;">
+    
+        <div style="color:{{textColor}}; width: 100%; text-align: center;font-family: Inter; font-size: 13px; font-weight: 400; line-height: 24px; text-align: left; text-underline-position: from-font; text-decoration-skip-ink: none;">
+            Mad cow is here
+        </div>
+
+        <svg id="bw-banner-close-icon" aria-hidden="true" viewBox="0 0 24 24" style="fill:{{textColor}}; font-size: 20px; cursor: pointer; width: 36px; height: 36px; padding: 12px;">
+            <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+        </svg>
+    
+</div>`;
+
 
 bw.banner={
     init:function(){
@@ -16,37 +21,25 @@ bw.banner={
         bw.banner.bindClick();
     },
     putHtml:function(){
-        const options = window.bwonboarddata.banner[0];
-        let option = {
-            ...bannerDefaultOptions,
-            ...options
-        };
-
-        let temp_html = `
-            <div id="bw-banner" style="position: fixed; top: 50px; z-index: 999999; height:50px; width:435px; background-color: #80e0ff; left: 50%; transform: translate(-50%, -50%);
-                line-height: 13px; font-weight: 400; display: flex; align-items: center; justify-content: space-between; padding: 0.7rem; border-radius: 5px;">
-                
-                    <div style="color: #2548ff; font-size: 20px; font-weight: bold; width: 100%; text-align: center;">
-                        Mad cow is here
-                    </div>
-          
-                    <svg id="bw-banner-close-icon" aria-hidden="true" viewBox="0 0 24 24" style="fill: #2548ff; font-size: 20px; cursor: pointer; width: 20px; height: 20px;">
-                        <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                    </svg>
-                
-            </div>`;
-        document.body.insertAdjacentHTML('afterbegin', temp_html);
-    },
-    putPlaceHolder: function(){
-        let temp_html = ``;
-        document.getElementById('bw-banner').insertAdjacentHTML('beforebegin', temp_html);
+        const bannersData = window.bwonboarddata.banner;
+     
+        let bannerHtml = '';
+        for (let i = 0; i < bannersData.length; i++) {
+            const item = bannersData[i];
+            let temp_html = ``;
+            temp_html = temp_banner_html.replace(new RegExp('{{backGroundColor}}') , item.backgroundColor);
+            temp_html = temp_html.replace(new RegExp('{{textColor}}'), item.fontColor);
+            
+            bannerHtml += temp_html;
+        }
+        document.body.insertAdjacentHTML('afterbegin', bannerHtml);
     },
     bindClick : function(){
         //todo: bind click event for close button to remove banner
         bw.util.bindLive("#bw-banner-close-icon", "click", function(e){
             setTimeout(function(){
-                e.target.parentElement.parentElement.remove();
-            }, 200);
+                e.target.parentElement.parentElement.style.display = 'none';
+            }, 300);
             
         })
     },
